77ffc0bb4ede74c8f725a345b74899e5a2a4884b
unknown <rodriguesnavil1@gmail.com>
2018-08-22 23:43:48 +0530

    some comment

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV ==='production'){
   
   module.exports = {mongodbURI:
-  'mongodb://navil:navil@123@ds229732.mlab.com:29732/todo-prod'
+  'mongodb://tododb:1qwerty@ds229732.mlab.com:29732/todo-prod'
   }
 }else{
   module.exports = {mongodbURI:
